196353dc03c1902068f8e72fc93510102f4f1b26
Thomas Cool <thomas.cool@velo3d.com>
2020-07-24 13:40:28 -0700

    Refactoring rotation Base rotation class now handles more of the work

--- Simulator/libSimulator/Rotation.h
+++ Simulator/libSimulator/Rotation.h
@@ -9,42 +9,57 @@ class Rotation {
   public:
     Rotation(TargetPtr source) : _source(source) {}
     
-    virtual std::optional<Second> getNextEventTime()=0;
-    virtual void resolveEventsUpToTime(const Second &time, const TargetPtr &)=0;
+    virtual std::optional<Second> getNextEventTime();
+    virtual void resolveEventsUpToTime(const Second &time, const TargetPtr &);
 
     const TargetPtr &getSource() const {return _source;}
+    void setTarget(TargetPtr &target){_target=target;}
+    [[nodiscard]] Second getNextFreeGCD() const{return _nextFreeGCD;}
+    [[nodiscard]] Second getNextFreeGCDForInstantCast() const{return _nextFreeGCDForInstant;}
+    virtual AbilityId getNextAbility() = 0;
+    void doRotation();
+    
     virtual ~Rotation()=default;
+
+    SIMULATOR_SET_MACRO(DelayAfterChanneled,Second,Second{0.0})
+    SIMULATOR_SET_MACRO(MinTimeAfterInstant,Second,Second{0.1})
+
+protected:
+    void setNextFreeGCD(Second time){
+        _nextFreeGCD=time;
+        _nextFreeGCDForInstant=time;
+    }
   private:
     TargetPtr _source;
+    TargetPtr _target{nullptr};
+    AbilityPtr _currentAbility{nullptr};
+    Second _nextFreeGCD{-1000.0};
+    Second _nextFreeGCDForInstant{-1000.0};
+    std::optional<Second> _lastEvent;
+    int _currentTick{0};
+    double _abilityAlacrityAmount{0.0};
+    Second _abilityCastTickTime{0.0};
+    Second _abilityStartTime{-1000.0};
+
 };
 
 class SetRotation : public Rotation{
   public:
     SetRotation(TargetPtr source,std::vector<AbilityId> ids) : Rotation(source),_ids(std::move(ids)) {}
     std::optional<Second> getNextEventTime() override;
-    void setStart(Second time){_nextFreeGCD=time;}
-    void resolveEventsUpToTime(const Second &time, const TargetPtr &) override;
-    void doRotation();
-    void setTarget(TargetPtr &target){_target=target;}
+    void setStart(Second time){setNextFreeGCD(time);}
     virtual ~SetRotation() = default;
     
-    SIMULATOR_SET_MACRO(DelayAfterChanneled,Second,Second{0.0})
     SIMULATOR_SET_MACRO(Repeats,int,1);
-    SIMULATOR_SET_MACRO(MinTimeAfterInstant,Second,Second{0.1})
+    AbilityId getNextAbility() override;
   private:
     AbilityIds _ids;
-    AbilityPtr _currentAbility{nullptr};
+    
     int _idCounter{0};
     int _iterationCount{0};
-    Second _nextFreeGCD{-1000.0};
-    Second _nextFreeGCDForInstant{-1000.0};
-    Second _abilityStartTime{-1000.0};
+
     
-    std::optional<Second> _lastEvent;
-    int _currentTick{0};
-    TargetPtr _target{nullptr};
-    double _abilityAlacrityAmount{0.0};
-    Second _abilityCastTickTime{0.0};
+
 
 
 };
