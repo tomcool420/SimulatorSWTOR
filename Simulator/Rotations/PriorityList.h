e1f3b57e14a15bd59b273b9150fe4aaec7064c4c
Thomas Cool <thomas.cool@me.com>
2020-07-28 13:41:47 -0700

    Rotation Testing

--- Simulator/Rotations/PriorityList.h
+++ Simulator/Rotations/PriorityList.h
@@ -16,20 +16,20 @@ using RotationalPriorityListPtr = std::shared_ptr<RotationalPriorityList>;
 
 class PriorityList : public RotationalPriorityList {
   public:
-    struct Priority {
-        std::variant<AbilityId, RotationalPriorityListPtr> action;
-        Conditions conditions;
-    };
-    void addAbility(AbilityId aId, Conditions &&conditions) { _priorites.push_back({aId, std::move(conditions)}); }
-    void addOtherId(RotationalPriorityListPtr rpl, Conditions &&conditions) {
-        _priorites.push_back({rpl, std::move(conditions)});
-    }
     [[nodiscard]] RotationalReturn getNextAbility(const TargetPtr &source, const TargetPtr &target,
                                                   const Second &nextInstant, const Second &nextGCD) override;
+    void addAbility(AbilityId aId, Conditions &&conditions) { _priorites.push_back({aId, std::move(conditions)}); }
+    void addPriorityList(RotationalPriorityListPtr rpl, Conditions &&conditions) {
+        _priorites.push_back({rpl, std::move(conditions)});
+    }
     void log(std::ostream &stream, int indent = 0) const override;
     virtual ~PriorityList() = default;
 
   private:
+    struct Priority {
+        std::variant<AbilityId, RotationalPriorityListPtr> action;
+        Conditions conditions;
+    };
     std::vector<Priority> _priorites;
 };
 } // namespace Simulator
---
+++ Simulator/Rotations/test/slightly_complex_rotation.cpp
@@ -0,0 +1,93 @@
+#include "../PriorityList.h"
+#include "../StaticRotation.h"
+#include <Simulator/Classes/DirtyFighting.h>
+#include <Simulator/libSimulator/constants.h>
+#include <Simulator/libSimulator/detail/log.h>
+#include <Simulator/libSimulator/detail/names.h>
+#include <gtest/gtest.h>
+
+using namespace Simulator;
+namespace {
+struct TestData {
+    TargetPtr source;
+    TargetPtr target;
+    std::shared_ptr<DirtyFighting> df;
+};
+TestData getTestData() {
+    RawStats rs;
+    rs.master = Mastery{4953};
+    rs.power = Power{2100};
+    rs.accuracyRating = AccuracyRating{1557};
+    rs.criticalRating = CriticalRating{313};
+    rs.alacrityRating = AlacrityRating{0};
+    rs.weaponDamageMH = {1376.0, 2556.0};
+    rs.weaponDamageOH = {1376.0 * 0.3, 2556.0 * 0.3};
+    rs.forceTechPower = FTPower{7008};
+    TestData ret;
+    ret.source = Target::New(rs);
+    ret.target = Target::New(rs);
+    ret.df = std::make_shared<DirtyFighting>();
+    return ret;
+}
+template <class K, class V> bool contains(const std::map<K, V> &m, const K &k) {
+    if (m.find(k) == m.end())
+        return false;
+    return true;
+}
+} // namespace
+TEST(Complex, test01) {
+    auto &&[s, t, c] = getTestData();
+    auto p = std::make_shared<PriorityList>();
+    p->addAbility(gunslinger_smugglers_luck, {getCooldownFinishedCondition(gunslinger_smugglers_luck)});
+    p->addAbility(gunslinger_hunker_down, {getCooldownFinishedCondition(gunslinger_hunker_down)});
+    p->addAbility(gunslinger_illegal_mods, {getCooldownFinishedCondition(gunslinger_illegal_mods)});
+    auto baseRotation = std::make_shared<StaticRotation>();
+    baseRotation->addAbility(gunslinger_vital_shot);
+    baseRotation->addAbility(dirty_fighting_shrap_bomb);
+    baseRotation->addAbility(dirty_fighting_dirty_blast);
+    baseRotation->addAbility(dirty_fighting_hemorraghing_blast);
+    baseRotation->addAbility(dirty_fighting_wounding_shots);
+    baseRotation->addAbility(dirty_fighting_dirty_blast);
+    baseRotation->addAbility(dirty_fighting_dirty_blast);
+    baseRotation->addAbility(dirty_fighting_dirty_blast);
+    baseRotation->addAbility(dirty_fighting_dirty_blast);
+    baseRotation->addAbility(dirty_fighting_wounding_shots);
+    p->addPriorityList(baseRotation, {});
+
+    DirtyFighting df;
+    df.setEstablishedFoothold(true);
+    df.setExploitedWeakness(true);
+
+    Second current{0.0};
+    std::map<AbilityId, int> counts;
+    for (int ii = 0; ii < 80; ++ii) {
+        auto na = p->getNextAbility(s, t, current, current);
+        if (auto a = std::get_if<AbilityId>(&na)) {
+            auto abl = df.getAbility(*a);
+            // SIM_INFO("Time: {}, abl: {} - {}", current.getValue(), *a, detail::getAbilityName(*a));
+            current += abl->getInfo().type == AbilityCastType::OffGCD ? Second(0.0) : Second(1.5);
+            if (abl->getCooldown() > Second(0.0))
+                s->setAbilityCooldown(*a, current + abl->getCooldown());
+            counts[*a] += 1;
+        } else {
+            // ASSERT_FALSE(true) << "Did not expect to encounter a delay in this test";
+        }
+    }
+    // for (auto &&abl : counts) {
+    //     std::cout << fmt::format("{{{},{}}}, //{}\n", abl.first, abl.second, detail::getAbilityName(abl.first));
+    // }
+    std::map<AbilityId, int> compare{
+        {760484089298944, 1},   // Illegal Mods
+        {807698664783872, 8},   // Shrap Bomb
+        {807711549685760, 14},  // Wounding Shots
+        {962811408678912, 2},   // Smuggler's Luck
+        {2044808159821824, 4},  // Hunker Down
+        {2115340112756736, 8},  // Vital Shot
+        {3404611985604608, 36}, // Dirty Blast
+        {3404620575539200, 7}   // Hemorraghing Blast
+    };
+    ASSERT_EQ(compare.size(), counts.size());
+    for (auto &&it : compare) {
+        ASSERT_EQ(counts.at(it.first), it.second);
+    }
+} // namespace
